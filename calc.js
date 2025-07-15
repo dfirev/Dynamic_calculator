e10a9b1ea7b83b1ba09fbf72d86e90dcd9f7f336
Diksha Verma <162664397+dfirev@users.noreply.github.com>
2025-07-15 11:57:07 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,10 @@
+function dis(n){
+    console.log(`Button ${n} pressed`)
+    document.getElementById("result").value+=n
+}
+function clr(){
+    document.getElementById("result").value = 0
+}
+function solve(){
+
+}
